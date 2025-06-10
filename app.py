fbab5909aa241422348a9396dfce1e1114431e38
raym293 <f20230905@goa.bits-pilani.ac.in>
2025-06-10 21:19:01 +0530

    latest

--- app.py
+++ app.py
@@ -137,7 +137,3 @@ async def update_note(note_id: int, note: Note):
 
 app.openapi()
 mcp.mount()
-if __name__ == "__main__":
-    import uvicorn
-    
-    uvicorn.run(app, host="127.0.0.1", port=8000)
